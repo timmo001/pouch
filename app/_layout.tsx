e0ca5c25ac949c7b3a1ef887407cf495e87b306a
Aidan Timson <aidan@timmo.dev>
2025-06-11 00:40:40 +0100

    react native reusables, nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,85 @@
+import "~/global.css";
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { Appearance, Platform } from "react-native";
+import { ThemeToggle } from "~/components/ThemeToggle";
+import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary
+} from "expo-router";
+
+const usePlatformSpecificSetup = Platform.select({
+  web: useSetWebBackgroundClassName,
+  android: useSetAndroidNavigationBar,
+  default: noop,
+});
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
 });
 
 export default function RootLayout() {
+  usePlatformSpecificSetup();
+  const { isDarkColorScheme } = useColorScheme();
+
   return (
     <ConvexProvider client={convex}>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "Pouch" }} />
-      </Stack>
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+        <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Pouch",
+              headerRight: () => <ThemeToggle />,
+            }}
+          />
+        </Stack>
+        <PortalHost />
+      </ThemeProvider>
     </ConvexProvider>
   );
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? React.useEffect
+    : React.useLayoutEffect;
+
+function useSetWebBackgroundClassName() {
+  useIsomorphicLayoutEffect(() => {
+    // Adds the background color to the html element to prevent white background on overscroll.
+    document.documentElement.classList.add("bg-background");
+  }, []);
+}
+
+function useSetAndroidNavigationBar() {
+  React.useLayoutEffect(() => {
+    setAndroidNavigationBar(Appearance.getColorScheme() ?? "light");
+  }, []);
+}
+
+function noop() {}
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,19 @@
-import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
+import { api } from "~/convex/_generated/api";
 
 export default function Index() {
   const groups = useQuery(api.groups.get);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {groups?.map(({ _id, name }) => <Text key={_id}>{name}</Text>)}
+    <View className="items-center justify-center flex-1">
+      <Text className="text-2xl font-bold text-white">Hello</Text>
+      {groups?.map(({ _id, name }) => (
+        <Text key={_id} className="text-2xl font-bold text-white">
+          {name}
+        </Text>
+      ))}
     </View>
   );
 }
