823bcf378e0bc48f3acfed2b190297e0f71dee68
Aidan Timson <aidan@timmo.dev>
2025-06-17 18:46:50 +0100

    A

--- web/src/app/(home)/layout.tsx
+++ web/src/app/(home)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { Authenticated, Unauthenticated } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "~/components/ui/button";
 
 export default function HomeLayout({
   children,
@@ -15,7 +16,12 @@ export default function HomeLayout({
         {children}
       </Authenticated>
       <Unauthenticated>
-        <SignInButton />
+        <div className="items-center justify-center flex-1 gap-4">
+          <p className="text-2xl font-bold text-center">Sign in to continue</p>
+          <Button size="lg" variant="default" asChild>
+            <SignInButton mode="modal" />
+          </Button>
+        </div>
       </Unauthenticated>
     </>
   );
