9c827758624b20601f6c200cc08ed74a7b96a7d7
Aidan Timson <aidan@timmo.dev>
2025-06-20 13:24:24 +0100

    Link

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-
+import Link from "next/link";
 import { useQuery } from "convex/react";
 import { Button } from "~/components/ui/button";
 import { api } from "~/convex/_generated/api";
+import { PlusIcon } from "lucide-react";
 
 export default function HomePage() {
   const groups = useQuery(api.groups.getAll);
@@ -10,8 +11,15 @@ export default function HomePage() {
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-2xl font-bold">Groups</h2>
-      <p className="text-sm text-muted-foreground">Total: {groups?.length}</p>
-      <Button>Create Group</Button>
+      <div className="flex flex-row gap-2 justify-between items-center px-2">
+        <p className="text-sm text-muted-foreground">Total: {groups?.length}</p>
+        <Link href="/groups/create" passHref>
+          <Button size="sm" variant="secondary">
+            <PlusIcon className="w-4 h-4" />
+            Create new
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
