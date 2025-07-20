f81c40556223fcbf7f5fda0e4e2738bcfb7378bc
Aidan Timson <aidan@timmo.dev>
2025-07-20 13:35:54 +0100

    Add not found page

---
+++ web/src/app/(home)/not-found.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+import { BreadcrumbsSetter } from "~/components/breadcrumbs/setter";
+
+export default function Loading() {
+  const router = useRouter();
+
+  return (
+    <>
+      <BreadcrumbsSetter
+        items={[
+          { key: "home", title: "Pouch", href: "/" },
+          { key: "not-found", title: "Not Found" },
+        ]}
+      />
+      <div className="flex h-[calc(100dvh-6rem)] w-full flex-col items-center justify-center gap-4">
+        <h1 className="text-3xl font-bold">Not Found</h1>
+        <p className="text-muted-foreground">
+          The page you are looking for does not exist.
+        </p>
+        <Button
+          size="lg"
+          onClick={() => {
+            router.back();
+          }}
+        >
+          Go Back
+        </Button>
+      </div>
+    </>
+  );
+}
