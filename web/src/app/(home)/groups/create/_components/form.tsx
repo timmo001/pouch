94a822fd97c18ca25449661216b5b21f23e07eba
Aidan Timson <aidan@timmo.dev>
2025-07-20 15:14:28 +0100

    Split

---
+++ web/src/app/(home)/groups/create/_components/form.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useConvexMutation } from "@convex-dev/react-query";
+import { z } from "zod";
+import { api } from "~/convex/_generated/api";
+import { type Id } from "~/convex/_generated/dataModel";
+import { Dots } from "~/components/ui/dots";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+const CreateGroupFormSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().optional(),
+});
+
+type CreateGroupForm = z.infer<typeof CreateGroupFormSchema>;
+
+export function CreateGroupForm() {
+  const { mutate, isPending } = useMutation({
+    mutationFn: useConvexMutation(api.groups.create),
+    onSuccess: (newId: Id<"groups">) => {
+      router.replace(`/groups/${newId}`);
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error("Failed to create group");
+    },
+  });
+  const router = useRouter();
+
+  const form = useForm<CreateGroupForm>({
+    resolver: zodResolver(CreateGroupFormSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  function onSubmit(values: CreateGroupForm) {
+    mutate(values);
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-3xl font-bold">Create Group</h1>
+      <Form {...form}>
+        <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description (optional)</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isPending}>
+            {isPending ? (
+              <span>
+                Creating
+                <Dots count={3} />
+              </span>
+            ) : (
+              "Create"
+            )}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- web/src/app/(home)/groups/create/page.tsx
+++ web/src/app/(home)/groups/create/page.tsx
@@ -1,63 +1,13 @@
-"use client";
 import { type Metadata } from "next";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { useConvexMutation } from "@convex-dev/react-query";
-import { z } from "zod";
-import { api } from "~/convex/_generated/api";
-import { type Id } from "~/convex/_generated/dataModel";
-import { Dots } from "~/components/ui/dots";
-import { Button } from "~/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-} from "~/components/ui/form";
-import { Input } from "~/components/ui/input";
 import { BreadcrumbsSetter } from "~/components/breadcrumbs/setter";
+import { CreateGroupForm } from "./_components/form";
 
 export const metadata: Metadata = {
   title: "Create Group",
   description: "Create a new group",
 };
 
-const CreateGroupFormSchema = z.object({
-  name: z.string().min(1),
-  description: z.string().optional(),
-});
-
-type CreateGroupForm = z.infer<typeof CreateGroupFormSchema>;
-
-export default function CreateGroupPage() {
-  const { mutate, isPending } = useMutation({
-    mutationFn: useConvexMutation(api.groups.create),
-    onSuccess: (newId: Id<"groups">) => {
-      router.replace(`/groups/${newId}`);
-    },
-    onError: (error) => {
-      console.error(error);
-      toast.error("Failed to create group");
-    },
-  });
-  const router = useRouter();
-
-  const form = useForm<CreateGroupForm>({
-    resolver: zodResolver(CreateGroupFormSchema),
-    defaultValues: {
-      name: "",
-      description: "",
-    },
-  });
-
-  function onSubmit(values: CreateGroupForm) {
-    mutate(values);
-  }
-
+export default async function CreateGroupPage() {
   return (
     <>
       <BreadcrumbsSetter
@@ -66,47 +16,8 @@ export default function CreateGroupPage() {
           { key: "groups/create", title: "Create Group" },
         ]}
       />
-      <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold">Create Group</h1>
-        <Form {...form}>
-          <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description (optional)</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Button type="submit" disabled={isPending}>
-              {isPending ? (
-                <span>
-                  Creating
-                  <Dots count={3} />
-                </span>
-              ) : (
-                "Create"
-              )}
-            </Button>
-          </form>
-        </Form>
-      </div>
+
+      <CreateGroupForm />
     </>
   );
 }
