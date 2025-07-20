32204348110cfd8a61be368a6ae0e6bbf73173a9
Aidan Timson <aidan@timmo.dev>
2025-07-20 11:20:14 +0100

    Placeholder

--- web/src/app/(home)/groups/[id]/page.tsx
+++ web/src/app/(home)/groups/[id]/page.tsx
@@ -1,3 +1,17 @@
-export default function GroupPage({ params }: { params: { id: string } }) {
-  return <div>{params.id}</div>;
+import { type Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  return { title: `Group ${params.id}` };
+}
+
+export default async function GroupPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return <div>Group: {params.id}</div>;
 }
