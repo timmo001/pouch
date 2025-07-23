392716f5693f4d5b9e4056544af1dd4f477c1951
Aidan Timson <aidan@timmo.dev>
2025-07-23 17:17:28 +0100

    Allow empty params for create page

--- src/app/(home)/create/page.tsx
+++ src/app/(home)/create/page.tsx
@@ -20,9 +20,6 @@ export default async function CreatePage({
 
   const { title, text, url } = await loadSearchParams(searchParams);
 
-  const hasValidQuery =
-    title?.length || text?.length || url?.length ? true : false;
-
   const value = url?.length ? url : text;
 
   const preloadedGroups = await preloadQuery(
@@ -45,32 +42,23 @@ export default async function CreatePage({
     <>
       <BreadcrumbsSetter items={[{ key: "create", title: "Create new..." }]} />
 
-      {hasValidQuery ? (
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row gap-2">
-            <h1 className="flex-grow text-3xl font-bold">
-              Create new List Item from {title}
-            </h1>
-          </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-row gap-2">
+          <h1 className="flex-grow text-3xl font-bold">
+            Create new List Item{" "}
+            {title || value ? ` from ${title ?? value}` : ""}
+          </h1>
+        </div>
 
-          <CreateListItemForm
-            preloadedGroups={preloadedGroups}
-            initialValues={{
-              type: value?.startsWith("http") ? "url" : "text",
-              value: value ?? "",
-              description: title ?? "",
-            }}
-          />
-        </div>
-      ) : (
-        <div className="flex flex-col gap-4">
-          <h1 className="text-3xl font-bold">Invalid query</h1>
-          <p className="text-sm text-muted-foreground">
-            Please provide a valid query.
-          </p>
-          <pre>{JSON.stringify({ title, text, url }, null, 2)}</pre>
-        </div>
-      )}
+        <CreateListItemForm
+          preloadedGroups={preloadedGroups}
+          initialValues={{
+            type: value?.startsWith("http") ? "url" : "text",
+            value: value ?? "",
+            description: title ?? "",
+          }}
+        />
+      </div>
     </>
   );
 }
