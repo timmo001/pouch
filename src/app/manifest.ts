4e27a6cd1321c4ed7101bae819faac6e4489323a
Aidan Timson <aidan@timmo.dev>
2025-07-23 15:08:24 +0100

    Add

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,14 +1,33 @@
-import type { MetadataRoute } from "next";
+import { type MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Pouch",
     short_name: "Pouch",
     description: "Store your favorite things",
+    categories: ["productivity", "organizer", "notes", "lists", "tasks"],
+    lang: "en-GB",
+    orientation: "any",
     start_url: "/",
     display: "standalone",
     background_color: "#1e1e2e",
     theme_color: "#cdd6f4",
+    shortcuts: [
+      {
+        name: "Home",
+        url: "/",
+        icons: [
+          { src: "/icon", sizes: "32x32", type: "image/png" },
+          { src: "/logo.svg", sizes: "any", type: "image/svg+xml" },
+        ],
+      },
+    ],
+    protocol_handlers: [
+      {
+        protocol: "pouch",
+        url: "/",
+      },
+    ],
     icons: [
       { src: "/logo.svg", sizes: "any", type: "image/svg+xml" },
       { src: "/icon", sizes: "32x32", type: "image/png" },
