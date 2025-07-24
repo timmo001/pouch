4d0136463013db36e1c8eea9c7fe9b002d0a898d
Aidan Timson <aidan@timmo.dev>
2025-07-24 19:40:31 +0100

    Use env

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,12 @@
-export default {
+import { env } from "~/env";
+
+const authConfig = {
   providers: [
     {
-      domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+      domain: env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
       applicationID: "convex",
     },
   ],
 };
+
+export default authConfig;
