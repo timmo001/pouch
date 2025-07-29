c9e30f17fdc0d996053c777c089db325429e12b7
Aidan Timson <aidan@timmo.dev>
2025-07-29 19:46:23 +0100

    Reduce sentry output

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -7,18 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://19bf9f8f9542f4b967fe5f929e70df19@o341827.ingest.us.sentry.io/4509703148470272",
 
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
-    }),
-  ],
-  // Enable logs to be sent to Sentry
-  _experiments: { enableLogs: true },
+  integrations: [Sentry.browserTracingIntegration()],
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for tracing.
   // We recommend adjusting this value in production
